cfc952dbd936d93ec17cc9612bc2cf8430ece31e
joeycarlos <jclaguio@gmail.com>
2019-10-29 18:21:08 -0700

    Add dialogue, dialogue trigger, dialogue manager

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private static DialogueManager _instance;
+
+    public static DialogueManager Instance {
+        get {
+            if (_instance == null) {
+                GameObject go = new GameObject("DialogueManager");
+                go.AddComponent<DialogueManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private Queue<string> sentences;
+
+    void Awake() {
+        _instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue) {
+        Debug.Log("Able to start conversation with " + dialogue.name);
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence() {
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+
+    void EndDialogue() {
+        Debug.Log("End conversation");
+    }
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void TriggerDialogue() {
+        DialogueManager.Instance.StartDialogue(dialogue);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+            DialogueManager.Instance.StartDialogue(dialogue);
+    }
+}
